981b46019a7633b1e0c9966ab64f9b9fac5a4a6c
AndreaBCaceres <bettincacere@gmail.com>
2021-03-12 17:50:54 -0500

    se agrego codigo js y se mevio una foto

---
+++ Ejercicio 4/codigo.js
@@ -0,0 +1,6 @@
+function sumar (){
+var n1 =  parseInt(document.getElementById("a").value);
+var n2 =  parseInt(document.getElementById("b").value);
+var r = n1 + n2; 
+document.getElementById("resultado").innerHTML = r; 
+}
